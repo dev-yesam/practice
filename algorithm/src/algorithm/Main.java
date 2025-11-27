af2eea96dd1a5f5f3a4ea8a47f1292b23b7df595
JI YECHAN <goldbuz17@gmail.com>
2025-11-27 21:59:41 +0900

    251127 BOJ 30802

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -1,33 +1,39 @@
 package algorithm;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// BOJ 4153
+		// BOJ 30802
 		Scanner sc = new Scanner(System.in);
 		
-		int[] arr = new int[3];
-		while (true) {
-			arr[0] = sc.nextInt();
-			arr[1] = sc.nextInt();
-			arr[2] = sc.nextInt();
-			
-			Arrays.sort(arr);
-			
-			// 종료 조건
-			if (arr[0]== 0 && arr[1] == 0 && arr[2] == 0 ) {
-				break;
-			}
-			
-			// 직각삼각형 판별
-			if (arr[2]*arr[2] == (arr[0]*arr[0] + arr[1]*arr[1])) {
-				System.out.println("right");
-			} else {
-				System.out.println("wrong");
+		int N = sc.nextInt();
+		
+		int[] sizes = new int[6];
+		for(int i = 0; i < 6; i++) {
+			sizes[i] = sc.nextInt();
+		}
+		
+		int tshirtsNumber = sc.nextInt();
+		int pensNumber = sc.nextInt();
+		
+		int minTshirtSetsNumber = 0;
+
+		for(int i = 0; i < 6; i++) {
+			minTshirtSetsNumber += sizes[i] / tshirtsNumber;
+			if (sizes[i] % tshirtsNumber != 0){
+				minTshirtSetsNumber ++;
 			}
 		}
+
+		
+		int maxPenSetNumber = N / pensNumber;
+		int junkPenNumber = N % pensNumber;
+		
+		System.out.println(minTshirtSetsNumber);
+		System.out.println(maxPenSetNumber + " " + junkPenNumber);
+		
 		sc.close();
+	
 	}
-}
+}	
