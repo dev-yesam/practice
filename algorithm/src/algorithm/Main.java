d3a2a31081666dd3f69bd70c17e791c39758051d
JI YECHAN <goldbuz17@gmail.com>
2025-11-27 20:49:10 +0900

    251127 algorithm start

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -1,10 +1,33 @@
 package algorithm;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		// BOJ 4153
+		Scanner sc = new Scanner(System.in);
 		
+		int[] arr = new int[3];
+		while (true) {
+			arr[0] = sc.nextInt();
+			arr[1] = sc.nextInt();
+			arr[2] = sc.nextInt();
+			
+			Arrays.sort(arr);
+			
+			// 종료 조건
+			if (arr[0]== 0 && arr[1] == 0 && arr[2] == 0 ) {
+				break;
+			}
+			
+			// 직각삼각형 판별
+			if (arr[2]*arr[2] == (arr[0]*arr[0] + arr[1]*arr[1])) {
+				System.out.println("right");
+			} else {
+				System.out.println("wrong");
+			}
+		}
+		sc.close();
 	}
 }
