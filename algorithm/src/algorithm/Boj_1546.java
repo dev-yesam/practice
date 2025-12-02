1f7487c89f27c077ad57367e644ddda9eee0d078
JI YECHAN <goldbuz17@gmail.com>
2025-12-03 08:39:44 +0900

    251203 boj 1246

---
+++ algorithm/src/algorithm/Boj_1546.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Boj_1546 {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int A[] = new int[N];
+		
+		
+		for(int i=0; i < A.length; i++) {
+			A[i] = sc.nextInt();
+		}
+		int sum = 0;
+		int max = 0;
+		
+		for(int i=0; i<A.length; i++) {
+			sum += A[i];
+			if(A[i] > max) {
+				max = A[i];
+			}
+		}
+		
+		System.out.println(sum * 100.0 / max /N);
+	}
+}
